b1d3b46f874e997eca1a48a134ec6fe93fca24a8
sssin1236 <suj0358@gmail.com>
2021-10-07 17:20:08 +0900

    플리커

--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
 const btnCall = document.querySelector(".btnMo");
 const menuMo = document.querySelector(".Mob");
-// const frame = document.querySelector("section");
-// const lists = frame.querySelectorAll("article");
-// const prev = document.querySelector(".btnPrev");
-// const next = document.querySelector(".btnNext");
-// const text = document.querySelector(".inner .txt");
-// const line = document.querySelector(".col");
-// const dots = line.querySelectorAll("span");
-// const col = document.querySelector("span div");
-// const deg = -72;
-// const len = lists.length-1;
-
-// let i = 0;
-// let num = 0;
-// let active = 0;
-
 
 btnCall.onclick = function(e){
     e.preventDefault();
@@ -23,54 +8,6 @@ btnCall.onclick = function(e){
     menuMo.classList.toggle("on");
 }
 
-// for(let el of lists){
-//     el.style.transform = `rotate(${deg * i}deg) translateX(-100vh)`;
-//     i++;
-// }
-
-// prev.addEventListener("click", ()=>{
-//     num++;
-//     frame.style.transform = `rotate(${num * deg}deg)`;
-
-//     if(active == 0){
-//         active = len;
-//     }else{
-//         active--;
-//     }
-
-//     activation(active, lists);
-//     activation2(active, dots);
-// });
-
-// next.addEventListener("click", ()=>{
-//     num--;
-//     frame.style.transform = `rotate(${num * deg}deg)`;
-
-//     if(active == 4){
-//         active = 0;
-//     }else{
-//         active++;
-//     }
-
-//     activation(active, lists);
-//     activation2(active, dots);
-// });
-
-// function activation(index, lists){
-//     for(let el of lists){
-//         el.classList.remove("on");
-//     }
-
-//     lists[index].classList.add("on");
-// }
-
-// function activation2(index, dots){
-//     for(let el of dots){
-//         el.classList.remove("on");
-//     }
-
-//     dots[index].classList.add("on");
-// }
 
 //--------------------------------jQuery----------------------------------------
 
@@ -128,26 +65,3 @@ $("#navi li a").on("click", function(e){
     }, 1000);
 });
 
-
-
-
-// function openWrap1(){
-//     $top.animate({width : "60%"}, speed, function(){
-//         $right.animate({height : "100%"}, speed, function(){
-//             $bottom.animate({width : "100%"}, speed, function(){
-//                 $left.animate({height : "65%"}, speed, function(){
-//                     $txt.animate({opacity : 0.9, left : "-130px"}, speed);
-//                 });
-//             });
-//         });
-//     });  
-// }
-
-// function closeWrap1(){
-//     $txt.animate({left : 0, opacity : 0}, speed/2, function(){
-//         $top.animate({width : 0}, speed/2);
-//         $right.animate({height : 0}, speed/2);
-//         $bottom.animate({width : 0}, speed/2);
-//         $left.animate({height : 0}, speed/2);
-//     });
-// }
---
+++ js/myFlickr.js
@@ -0,0 +1,60 @@
+
+$.ajax({
+    url:"https://www.flickr.com/services/rest/?method=flickr.photos.search",
+    dataType:"json",
+    data:{
+        api_key:"645bbdf320519f9a5473a288fc9f617a",
+        per_page:30,
+        format:"json",
+        nojsoncallback:1,
+        privacy_filter: 5,
+        tags:"building" 
+    }
+})
+.success(function(data){
+    console.log(data.photos.photo);
+
+    let items = data.photos.photo;
+
+    $("#gallery").append("<ul>");
+
+    $(items).each(function(index, data){
+        $("#gallery ul").append(
+            $("<li>")
+                .append(
+                    $("<a>").attr({
+                        href : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_b.jpg"
+                    })
+                    .append(
+                        $("<img>").attr({
+                            src : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_b.jpg"
+                        })
+                    )
+                )
+                .append(
+                    $("<h2>").text("Lorem ipsum.")
+                )
+        )}
+    );
+})
+.error(function(err){
+    console.err("데이터를 호출하는 데 실패했습니다.");
+})
+
+$("body").on("click", "#gallery ul li", function(e){
+    e.preventDefault();
+
+    let imgAdd = $(this).children("a").attr("href");
+
+    $("body").append(
+        $("<div class='pop'>").append(
+            $("<img>").attr({ src : imgAdd }),
+            $("<span>").text("Close")
+        )
+    )
+});
+
+
+$("body").on("click", ".pop span", function(){
+    $(".pop").remove();
+});
