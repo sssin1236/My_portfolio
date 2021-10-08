786f793ed1f929df38ae83e3e426261f7639758b
sssin1236 <suj0358@gmail.com>
2021-10-08 17:47:51 +0900

    영상 업로드

---
+++ js/youtube.js
@@ -0,0 +1,46 @@
+
+$.ajax({
+    url: "https://www.googleapis.com/youtube/v3/playlistItems",
+    dataType : 'jsonp',
+    data :{
+        part : "snippet",
+        key : "AIzaSyCCiZuUxyRnAnWNnLdQxnZ5COuFx0Cv33A",
+        maxResults : 6,
+        playlistId : "PLlgbG45RVNao7w0WwmJlvh1umeP_ol2r6"
+    }
+})
+.success(function(data){
+    
+    let items = data.items;
+    console.log(items);
+
+    $(items).each(function(index, data){
+        
+        let txt = data.snippet.description;
+        let len = txt.length;
+
+        if(len > 200){
+            txt = txt.substr(0, 200) + "...";
+        }
+
+        let date = data.snippet.publishedAt;
+        date = date.split("T")[0];
+
+        $("#video").append(
+            $("<article>").append(
+                $("<a>").attr({ href : data.snippet.resourceId.videoId }).append(
+                    $("<img>").attr({ src : data.snippet.thumbnails.high.url})
+                ),
+                $("<div class='con'>").append(
+                    $("<h2>").text(data.snippet.title),
+                    $("<p>").text(txt),
+                    $("<span>").text(date)
+                )
+            )
+        )
+    })
+
+})
+.error(function(err){
+    console.error(err);
+})
