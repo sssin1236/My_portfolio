8aae59a2077efbe817fac4a90d2159e6876ced2f
sssin1236 <suj0358@gmail.com>
2021-10-12 23:48:18 +0900

    컨트롤 박스

--- js/map.js
+++ js/map.js
@@ -27,3 +27,13 @@ t_off.addEventListener("click", function(e){
     t_on.classList.remove("on");
     t_off.classList.add("on");
 });
+
+var mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
