e39b70c9da50f7aa425629f12cc66e5b5997d388
sssin1236 <suj0358@gmail.com>
2021-10-12 23:46:50 +0900

    교통정보

--- js/map.js
+++ js/map.js
@@ -5,3 +5,25 @@ var options = {
 };
 
 var map = new kakao.maps.Map(container, options); 
+
+
+const t_on = document.querySelectorAll(".traffic li")[0];
+const t_off = document.querySelectorAll(".traffic li")[1];
+
+t_on.addEventListener("click", function(e){
+    e.preventDefault();
+
+    map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);    
+
+    t_on.classList.add("on");
+    t_off.classList.remove("on");
+});
+
+t_off.addEventListener("click", function(e){
+    e.preventDefault();
+
+    map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+
+    t_on.classList.remove("on");
+    t_off.classList.add("on");
+});
