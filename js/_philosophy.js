b317a52294009dc742d31ff288b0f0e16766714e
sssin1236 <suj0358@gmail.com>
2021-10-12 13:34:27 +0900

    스크롤 추가

---
+++ js/_philosophy.js
@@ -0,0 +1,39 @@
+const btnCall = document.querySelector(".btnMo");
+const menuMo = document.querySelector(".Mob");
+
+btnCall.onclick = function(e){
+    e.preventDefault();
+
+    btnCall.classList.toggle("on");
+    menuMo.classList.toggle("on");
+}
+
+//-------------------------------------------------------------------------------
+
+const $boxs = $(".myScroll");
+
+let posArr = [];
+let len = $boxs.length;
+let plusLine = -200;
+
+for(let i=0; i<len; i++){
+    posArr.push($boxs.eq(i).offset().top);
+}
+
+$(window).on("resize", function(){
+    posArr = [];
+    for(let i=0; i<len; i++){
+        posArr.push($boxs.eq(i).offset().top);
+    }
+});
+
+$(window).on("scroll", function(){
+    let scroll = $(this).scrollTop();
+
+    for(let i=0; i<len; i++){
+        if(scroll >= posArr[i] + plusLine){
+            $boxs.removeClass("on");
+            $boxs.eq(i).addClass("on");
+        }
+    }
+});
