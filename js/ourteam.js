f718ed7dc93e3f4534ae6d5c8cbb91385820c00e
sssin1236 <suj0358@gmail.com>
2021-10-21 00:52:02 +0900

    멤버 완성

---
+++ js/ourteam.js
@@ -0,0 +1,26 @@
+const btnCall = document.querySelector(".btnMo");
+const menuMo = document.querySelector(".Mob");
+
+btnCall.onclick = function(e){
+    e.preventDefault();
+
+    btnCall.classList.toggle("on");
+    menuMo.classList.toggle("on");
+}
+
+const $mem = $(".mem");
+const $article = $mem.find("article");
+
+
+$(".career").on("click", function(){
+
+    let has = $(this).hasClass("on");
+    if(has){
+        $(this).removeClass("on");
+        $(this).parent().siblings(".pic").css({opacity: 1});
+    }else{
+        $(this).addClass("on");
+        $(this).parent().siblings(".pic").css({opacity: 0});
+    }
+    
+});
