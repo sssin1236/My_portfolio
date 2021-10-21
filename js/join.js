5799932203ff0e1c5b00d90e1fa11b37b2361511
sssin1236 <suj0358@gmail.com>
2021-10-21 23:10:44 +0900

    policy 완성

---
+++ js/join.js
@@ -0,0 +1,16 @@
+
+$("dt").on("click", function(){
+
+    let isOn = $(this).hasClass("on");
+    
+    $("dt").removeClass("on");
+    $("dt").next().slideUp(500);
+
+    if(isOn){
+        $(this).removeClass("on");
+        $(this).next().slideUp(500);
+    }else{
+        $(this).addClass("on");
+        $(this).next().slideDown(500);
+    }
+});
