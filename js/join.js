a5524c40e9051b9fb941ef691c1736378b14b0c7
DESKTOP-ONC0LLR\82107 <suj0358@gmail.com>
2021-11-01 17:54:02 +0900

    비밀번호 스크립트 수정

--- js/join.js
+++ js/join.js
@@ -29,7 +29,9 @@ $("input[type=submit]").on("click", function(e){
     e.preventDefault();
 
     if(!isID('userid', 4, 10)) e.preventDefault();
-    if(!isPwd('pwd1', 'pwd2', 8, 15)) e.preventDefault();
+    if(!isPwd('pwd1', 8, 15)) e.preventDefault();
+    if(!isPwd2('pwd1', 'pwd2')) e.preventDefault();
+    if(!phon('call')) e.preventDefault();
 });
 
 
@@ -55,10 +57,18 @@ $("input[name=pwd1]").on("keyup", function(){
     let eng = /[a-zA-Z]/;
     let spc = /[~!@#$%^&*()_+|\[\]-]/;
 
-    if(txt.length >= len && txt.length < len2 && eng.test(pwd1)){
+    if(txt.length >= len && txt.length < len2 && eng.test(txt)){
         $(this).parent().find("p").eq(0).css({ color: "#111"});
     }else{
         $(this).parent().find("p").eq(0).css({ color: "rgb(255, 91, 32)"});
+    }if(spc.test(txt)){
+        $(this).parent().find("p").eq(1).css({ color: "#111"});
+    }else{
+        $(this).parent().find("p").eq(1).css({ color: "rgb(255, 91, 32)"});
+    }if(num.test(txt)){
+        $(this).parent().find("p").eq(2).css({ color: "#111"});
+    }else{
+        $(this).parent().find("p").eq(2).css({ color: "rgb(255, 91, 32)"});
     }
 
     
@@ -84,25 +94,61 @@ function isID(name, len, len2){
 }
 
 
-function isPwd(name1, name2, len, len2){
+function isPwd(name, len, len2){
 
     if(len == undefined) len =8;
     if(len2 == undefined) len2 =15;
 
-    let pwd1 = $("input[name="+name1+"]").val();
-    let pwd2 = $("input[name="+name2+"]").val();
-    let targetPos = $("input[name="+name1+"]").offset().top + -500;
+    let pwd1 = $("input[name="+name+"]").val();
+    let targetPos = $("input[name="+name+"]").offset().top + -500;
 
     let num = /[0-9]/;
     let eng = /[a-zA-Z]/;
     let spc = /[~!@#$%^&*()_+|\[\]-]/;
 
-    if(pwd1 === pwd2 && pwd1.length >=len && num.test(pwd1) && eng.test(pwd1) && spc.test(pwd1)){
-        $("input[name="+name1+"]").parent().find("p").css({ color: "#111"});
+    if(pwd1.length >=len && num.test(pwd1) && eng.test(pwd1) && spc.test(pwd1)){
+        $("input[name="+name+"]").parent().find("p").css({ color: "#111"});
         return true;
     }else{
         $("html, body").animate({ scrollTop : targetPos }, 500);
-        $("input[name="+name1+"]").focus();
+        $("input[name="+name+"]").focus();
+        return false;
+    }
+}
+
+function isPwd2(name1, name2){
+    let pwd1 = $("input[name="+name1+"]").val();
+    let pwd2 = $("input[name="+name2+"]").val();
+    let targetPos = $("input[name="+name2+"]").offset().top + -500;
+
+    if(pwd1 === pwd2){
+        $("input[name="+name2+"]").parent().find("p").remove();
+        return true;
+    }else{
+        $("input[name"+name2+"]").parent().find("p").remove(); 
+        $("input[name="+name2+"]").parent().append(
+            "<p>비밀번호를 동일하게 입력해주세요.</p>"
+        )
+        $("html, body").animate({ scrollTop : targetPos }, 500);
+        $("input[name="+name2+"]").focus();
+        return false;
+    }
+}
+
+function phon(name){
+    let call = $("input[name="+name+"]").val();
+    let sel = $("select[name"+name+"]").children("option:selected").val(); 
+    let targetPos = $("input[name="+name+"]").offset().top + -500;
+    let ph = $("input[name="+name+"]").attr("placeholder");
+    let num = /[0-9]/;
+
+    if(sel !=="" && num.test(call)){
+        return true;
+    }else{
+        $("input[name"+name+"]").empty();
+        $("input[name="+name+"]").attr({placeholder : "연락처를 정확하게 입력해주세요."});
+        $("html, body").animate({ scrollTop : targetPos }, 500);
+        $("input[name="+name+"]").focus();
         return false;
     }
 }
