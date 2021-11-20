ddac3f77682fb5b804f1a188c9120e0e62d9da51
sssin1236 <suj0358@gmail.com>
2021-11-20 23:09:42 +0900

    youtube 함수 패키징

--- js/youtube.js
+++ js/youtube.js
@@ -32,55 +32,59 @@ $("dt").on("click", function(){
     }
 });
 
-$.ajax({
-    url: "https://www.googleapis.com/youtube/v3/playlistItems",
-    dataType : 'jsonp',
-    data :{
-        part : "snippet",
-        key : "AIzaSyCCiZuUxyRnAnWNnLdQxnZ5COuFx0Cv33A",
-        maxResults : 6,
-        playlistId : "PLlgbG45RVNao7w0WwmJlvh1umeP_ol2r6"
-    }
-})
-.success(function(data){
+
+
+function getYoutube(opt){
+    $.ajax({
+        url: "https://www.googleapis.com/youtube/v3/playlistItems",
+        dataType : 'jsonp',
+        data :{
+            part : "snippet",
+            key : "AIzaSyCCiZuUxyRnAnWNnLdQxnZ5COuFx0Cv33A",
+            maxResults : 6,
+            playlistId : "PLlgbG45RVNao7w0WwmJlvh1umeP_ol2r6"
+        }
+    })
+    .success(function(data){
+        
+        let items = data.items;
+        console.log(items);
     
-    let items = data.items;
-    console.log(items);
-
-    $(items).each(function(index, data){
-        
-        let txt = data.snippet.description;
-        let len = txt.length;
-        let tit = data.snippet.title;
-        let titLen = tit.length;
-        
-
-        if(len > 75){
-            txt = txt.substr(0, 75) + "...";
-        }
-
-        if(titLen > 40){
-            tit = tit.substr(0, 40) + "...";
-        }
-
-        $("#video").append(
-            $("<article>").append(
-                $("<a>").attr({ href : data.snippet.resourceId.videoId }).append(
-                    $("<img>").attr({ src : data.snippet.thumbnails.high.url})
-                ),
-                $("<div class='con'>").append(
-                    $("<span>").text(data.snippet.videoOwnerChannelTitle),
-                    $("<h2>").text(tit),
-                    $("<p>").text(txt)
+        $(items).each(function(index, data){
+            
+            let txt = data.snippet.description;
+            let len = txt.length;
+            let tit = data.snippet.title;
+            let titLen = tit.length;
+            
+    
+            if(len > 75){
+                txt = txt.substr(0, 75) + "...";
+            }
+    
+            if(titLen > 40){
+                tit = tit.substr(0, 40) + "...";
+            }
+    
+            $("#video").append(
+                $("<article>").append(
+                    $("<a>").attr({ href : data.snippet.resourceId.videoId }).append(
+                        $("<img>").attr({ src : data.snippet.thumbnails.high.url})
+                    ),
+                    $("<div class='con'>").append(
+                        $("<span>").text(data.snippet.videoOwnerChannelTitle),
+                        $("<h2>").text(tit),
+                        $("<p>").text(txt)
+                    )
                 )
             )
-        )
+        })
+    
     })
-
-})
-.error(function(err){
-    console.error(err);
-})
+    .error(function(err){
+        console.error(err);
+    })
+}
 
 $("body").on("click", "#video article a", function(e){
     e.preventDefault();
